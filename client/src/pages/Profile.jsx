649cf4ff61e38ae75a45c94b9062ea66f1a08576
ARYANROY20 <aryanroy200606@gmail.com>
2026-07-13 12:11:10 +0530

    Update discovery and user card

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Profile = () => {
+  const {profileId}=useParams()
+
+  const [user,setUser]=useState(null)
+  const [posts,setPosts]=useState([])
+
   return (
     <div>
       
