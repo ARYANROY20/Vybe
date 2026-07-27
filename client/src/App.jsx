7e8011321e5d3e31e7c320fa771e9e10384fd79b
ARYANROY20 <aryanroy200606@gmail.com>
2026-07-27 17:36:19 +0530

    Add real-time messaging updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useRef } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "./pages/Login";
 import Feed from "./pages/Feed";
 import Messages from "./pages/Messages";
@@ -15,11 +15,18 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchUser } from "./features/user/userSlice";
 import { fetchConnections } from "./features/connections/connectionsSlice";
+import { addMessage } from "./features/messages/messagesSlice";
+import toast from "react-hot-toast";
+import Notification from "./components/Notification";
 
 
 const App = () => {
   const {user}= useUser()
   const {getToken} = useAuth()
+
+  const {pathname}=useLocation()
+  const pathnameRef = useRef(pathname)
+
   const dispatch=useDispatch()
 
   useEffect(()=>{
@@ -32,6 +39,32 @@ const App = () => {
     }
     fetchData()
   },[user,getToken,dispatch])
+
+  useEffect(()=>{
+    pathnameRef.current=pathname
+  },[pathname])
+
+  useEffect(()=>{
+    if(user){
+      const eventSource=new EventSource(import.meta.env.VITE_BASEURL + '/api/message/' + user.id);
+
+      eventSource.onmessage=(event)=>{
+        const message = JSON.parse(event.data)
+
+        if(pathnameRef.current === ('/messages/' + message.from_user_id._id)){
+          dispatch(addMessage(message))
+        }else{
+          toast.custom((t)=>(
+            <Notification t={toast} message={message}/>
+          ),{position:'bottom-right',duration:5000})
+        }
+      }
+      return ()=>{
+        eventSource.close()
+      }
+    }
+  },[user,dispatch])
+
   return (
     <>
     <Toaster />
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -23,7 +23,7 @@ const Layout = () => {
         sidebarOpen ? 
         <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=>{
             setSidebarOpen(false)
-        } } /> : <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow-w-10 h-10 text-gray-600 sm:hidden' onClick={()=>{
+        } } /> : <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=>{
             setSidebarOpen(true)
         } }/>
       }
